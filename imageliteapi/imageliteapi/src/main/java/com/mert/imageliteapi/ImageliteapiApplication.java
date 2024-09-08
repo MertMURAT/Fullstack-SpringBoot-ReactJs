7f431c3eb237fa2745f2deddd32e1bea6b9455e9
Mert MURAT <mert.murat2@hotmail.com>
2024-09-08 19:01:22 +0300

    Image Repository added and ımage model saved database with command line runner

--- imageliteapi/imageliteapi/src/main/java/com/mert/imageliteapi/ImageliteapiApplication.java
+++ imageliteapi/imageliteapi/src/main/java/com/mert/imageliteapi/ImageliteapiApplication.java
@@ -1,7 +1,13 @@
 package com.mert.imageliteapi;
 
+import com.mert.imageliteapi.base.repository.ImageRepository;
+import com.mert.imageliteapi.domain.entity.Image;
+import com.mert.imageliteapi.domain.enums.ImageExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,4 +18,16 @@ public class ImageliteapiApplication {
 		SpringApplication.run(ImageliteapiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ImageRepository imageRepository) {
+		return args -> {
+			Image image = Image.builder()
+					.name("image")
+					.size(100L)
+					.extension(ImageExtension.PNG)
+					.tags("tag1, tag2")
+					.build();
+			imageRepository.save(image);
+		};
+	}
 }
