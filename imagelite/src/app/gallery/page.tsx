e799c6932ec9f4230bcd43bbeb2dcbdaa71180b4
Mert MURAT <mert.murat2@hotmail.com>
2024-09-29 16:34:43 +0300

    Processing of images in the gallery

--- imagelite/src/app/gallery/page.tsx
+++ imagelite/src/app/gallery/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Template, ImageCard } from "@/components/Index";
+import { Template, ImageCard } from "@/components";
 import { Image } from "@/resources/image/image.resources";
 import { useImageService } from "@/resources/image/image.service";
 import { useState } from "react";
@@ -12,6 +12,24 @@ export default function GalleryPages() {
         const result = await userService.search();
         setImages(result);
         console.table(result);
+        console.table("IMAGES : " + images);
+    }
+
+    function renderImageCard(image: Image) {
+        return (
+            <div>
+                <ImageCard
+                    src={image.url ? image.url : "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg"}
+                    name={image.name}
+                    size={image.size}
+                    dataUpload={image.uploadDate} />
+            </div>
+        );
+    }
+
+    function renderImageCards() {
+        // return images.map(image => renderImageCard(image));
+        return images.map(renderImageCard);
     }
 
     return (
@@ -19,11 +37,10 @@ export default function GalleryPages() {
             <button className="bg-gray-500" onClick={searchImages} >SEARCH</button>
             <h4 className="font-bold text-3xl py-2">Gallery</h4>
             <section className="grid grid-cols-4 gap-8">
-                <ImageCard
-                    src="https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg"
-                    name="Kuş"
-                    size="10MB"
-                    dataUpload="01/01/2024" />
+                {
+                    renderImageCards()
+                }
+
 
             </section>
         </Template>
